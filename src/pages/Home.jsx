a11aa82a2f531fcd8d18701f1032b6aafbb9b412
apesconsole <apesconsole@gmail.com>
2026-01-02 17:28:47 +0530

    Service lik fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,16 +11,22 @@ export default function Home() {
       <div className="relative z-10">
         <h2 className="text-4xl md:text-6xl font-bold">Bringing Your Vision to Life</h2>
         <p className="mt-4 text-lg md:text-xl">Our aim is to make organization more data driven, generating insights from less traveled paths of Data Analytics and Social Intelligence</p>
-        <a
-          href="#services"
-          className="mt-6 inline-block px-6 py-3 rounded-full border-4 border-gradient-to-r from-purple-500 via-pink-500 to-red-500
+        
+              <button
+                onClick={() =>
+                  document
+                    .getElementById("services")
+                    ?.scrollIntoView({ behavior: "smooth" })
+                }
+                className="mt-6 inline-block px-6 py-3 rounded-full border-4 border-gradient-to-r from-purple-500 via-pink-500 to-red-500
                     bg-white bg-clip-text
                     bg-gradient-to-r from-purple-500 via-pink-500 to-red-500
                     hover:from-green-400 hover:via-blue-500 hover:to-purple-600
                     transition-all duration-300"
-        >
-          Our Services
-        </a>
+              >
+               Our Services
+              </button>
+          
 
       </div>
     </section>
